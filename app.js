5e478b1bc8cb7ce547efb094b37471ffc51de4f5
KumanEnrique <36672948+KumanEnrique@users.noreply.github.com>
2021-10-28 15:48:29 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 (async()=>{
+    let contenedorArray = []
     const resp = await fetch('https://api.github.com/users/KumanEnrique')
     const data = await resp.json()
     const amountRepos = data.public_repos
+    console.log(amountRepos)
+    const pagesNumber = Math.ceil(amountRepos  / 100 )
+    console.log(pagesNumber)
     const URL_REPOS = data.repos_url
-    //const resp2 = await fetch(URL_REPOS+"?per_page=123&page=2")
-    const resp2 = await fetch(URL_REPOS+"?per_page=123&page=2")
-    const data2 = await resp2.json()
-    //console.log(data2)
-    //const a = data2.map((elemento)=> elemento.created_at = new Date(elemento.created_at).toLocaleDateString() )
-    const arreglo = data2.map((elemento,index,array)=> {
-        elemento.created_at = new Date(elemento.created_at)
-        return elemento
-    })
-    console.log(arreglo.sort((a, b) => b.created_at - a.created_at  ) );
+    for(let i = 1;i < pagesNumber + 1;i++){
+        const resp2 = await fetch(URL_REPOS+`?per_page=${amountRepos}&page=${i}`)
+        const data2 = await resp2.json()
+        const arreglo = data2.map((elemento,index,array)=> {
+            elemento.created_at = new Date(elemento.created_at)
+            return elemento
+        })
+        contenedorArray = [...contenedorArray,...arreglo]
+    }
+    
+
+console.log(contenedorArray)
+    console.log(contenedorArray.sort((a, b) => b.created_at - a.created_at  ) );
 })();
